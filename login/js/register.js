f8a67be848c5051cbab2f35d09c7165911ffe587
nnstar-rin <nnurinsajida@gmail.com>
2026-05-16 18:26:55 +0700

    Update registration success and error messages

--- login/js/register.js
+++ login/js/register.js
@@ -16,9 +16,9 @@ document.getElementById("registerForm").addEventListener("submit", async functio
     const data = await res.json();
 
     if (data.status === "success") {
-        alert("Registrasi berhasil!");
+        document.getElementById("message").innerText = "Registrasi berhasil, silakan login";
         window.location.href = "index.html";
     } else {
-        alert(data.message || "Gagal registrasi");
+        document.getElementById("message").innerText = data.message || "Gagal registrasi";
     }
 });
