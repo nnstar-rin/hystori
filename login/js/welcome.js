f7e2fdba6b87ff9f7d72f1d70a754b3aced2a04c
nnstar-rin <nnurinsajida@gmail.com>
2026-05-17 03:50:41 +0700

    Refactor login/logout functions and button text

--- login/js/welcome.js
+++ login/js/welcome.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    function goLogin() {
+        window.location.href = "login/index.html";
+    }
+
+    function logout() {
+        localStorage.removeItem("username");
+        location.reload();
+    }
+
     const user = localStorage.getItem("username");
 
     if (user) {
         document.getElementById("userInfo").innerText = "Halo, " + user;
 
         document.getElementById("authArea").innerHTML = `
-            <button onclick="logout()" class="nav-cta">LOGOUT</button>
+            <button onclick="logout()" class="nav-cta">Logout</button>
         `;
     }
-});
 
-function logout() {
-    localStorage.removeItem("username");
-    localStorage.removeItem("isLogin");
-    location.reload();
-}
+    window.goLogin = goLogin;
+    window.logout = logout;
+
+});
