611cda2aeda98e4c9f3fb8510ed7701c0166ccee
nnstar-rin <nnurinsajida@gmail.com>
2026-05-18 03:50:58 +0700

    Fix formatting issues in login.js

--- login/js/login.js
+++ login/js/login.js
@@ -1,4 +1,4 @@
-document.getElementById("loginForm").addEventListener("submit", async function(e) {
+ocument.getElementById("loginForm").addEventListener("submit", async function(e) {
     e.preventDefault();
 
     const username = document.getElementById("username").value.trim();
